9d422bd43fbee166ea6b50bfb10fa1aae4187114
wgartg <wgartger@gmail.com>
2026-07-19 21:44:49 +0900

    Add files via upload

--- js/auth-config.js
+++ js/auth-config.js
@@ -5,3 +5,7 @@ export const DISCORD_CLIENT_ID = "1528358100329828442";
 
 // Cloudflare Workerをデプロイした後のURL + パス。
 export const AUTH_WORKER_URL = "https://wasshoi-discord-auth.cook-king.workers.dev/auth/discord";
+
+// Discordなどにリンクを貼った時に写真とレシピ名がプレビュー表示されるようにするための、
+// Worker側の共有ページのベースURL(末尾スラッシュなし)。AUTH_WORKER_URLと同じドメインです。
+export const SHARE_LINK_BASE = "https://wasshoi-discord-auth.cook-king.workers.dev";
